b0196df13833ec8fd428f76cc2d0fbb88da1134e
kebilly <www538huii@gmail.com>
2024-02-07 00:50:38 +0800

    Java and Kotlin variable,data type

--- src/main/java/org/billy/Hello.java
+++ src/main/java/org/billy/Hello.java
@@ -4,7 +4,22 @@ public class Hello {
     public static  void main(String[] args){
 //        System.out.println("Hello!");
         /*sout*/
+        //8個基本資料型態
         Person p = new Person();
         p.hello();
+        int age = 19;
+        Integer  age2 = 19;
+        char c = 'A';
+        char d = '我';
+        Character c2 = 'B';
+        byte b = 120;
+        float weight = 66.5f;
+        Float w = 70.5f;
+        double height = 1.7;
+        Double hei = 1.8;
+        boolean  adult = true;
+        boolean enroll = false;
+        Boolean e =  true;
+        String name = "Tom";
     }
 }
--- src/main/java/org/billy/Hello.kt
+++ src/main/java/org/billy/Hello.kt
@@ -3,12 +3,22 @@ package org.billy
 fun main() {
 //    println("Hello Kotlin");
 //    Human().hello()
+//    Kotlin沒有基本資料型態
     val h  = Human()
     h.hello()
+    val age : Int = 19
+    val age2 = 20
+    var weight = 66.5
+    var weight2 = 66.5f
+    var name : String
+    name = "Hank"
+
+
 }
 
-//val(value)代表當我定義完這個物件後就不能再修改他的值了
-
+//val(value)代表當我定義完這個物件後就不能再修改他的值了，不可變的
+//var(varible)值可以改變的
+//vak 名稱: 型態=值
 
 class Human{
     fun hello() {
